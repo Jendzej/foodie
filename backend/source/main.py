73a76356e1189e2dee5b129df2cbca66bfa4ea66
Jedrzej Runowicz <jedrzej.runowicz@gmail.com>
2023-02-23 22:49:16 +0100

    Init: Creating first files and directories

---
+++ backend/source/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "PUT", "DELETE"],
+    allow_headers=["*"]
+)
